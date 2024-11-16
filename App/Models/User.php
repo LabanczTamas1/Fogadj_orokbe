d88f5fc5dcec4d0d7910457498823ab128be8baf
theoneandonlyub <theoneandonlyub@proton.me>
2024-11-16 02:28:55 +0100

    CRUD Update - felhasználó jelszó módosítás funkció feltöltése.

--- App/Models/User.php
+++ App/Models/User.php
@@ -15,4 +15,26 @@ class User extends Model
         'type' => 'string',
         'shelter_name' => 'string'
     ];
+
+    public function updateUserPassword($post){
+        if($post){
+            $data['password'] =  \App\Tools::Crypt($post['password']);
+            $this->set('password', $data['password']);
+            try {
+                if ($this->update()) {
+                    $destroy = new SessionController;
+                    $destroy->destroy();
+                    header("Location:/");
+                    \App\Tools::FlashMessage('Sikeresen megváltoztatta jelszavát.', 'success');
+                }
+            } catch (\Exception $e) {
+                \App\Tools::FlashMessage("Valami hiba történt.");
+                echo $e;
+                return false;
+            }
+        } else {
+            \App\Tools::FlashMessage('Hiba.', 'danger');
+            return false;
+        }
+    }
 }
