aa8e5c5c88ba47a89254b01d360a7914ce6c725f
theoneandonlyub <theoneandonlyub@proton.me>
2024-11-17 21:48:57 +0100

    User interface uploads feltöltése.

--- user/uploads.php
+++ user/uploads.php
@@ -1,7 +1,19 @@
 <?php
 require_once __DIR__ . '/../lib/autoload.php';
-include __DIR__ . '/../views/components/card.php';
-new App\Template('empty');
+new App\Template('uploads','empty');
+use App\Helper;
+if(!Helper::isAuth() && App\Helper::user()->type != 'Shelter'){
+    header('Location: /');
+}
+
+$shelterModel = new App\Models\Shelter;
+$petModel = new App\Models\Pet;
+$userPets = $petModel->getItemsBy('user_id',Helper::user()->id);
+$userShelters = $shelterModel->getItemsBy('user_id',Helper::user()->id);
+$userPets = $petModel->getItemsBy('user_id',Helper::user()->id);
+require_once '../views/components/shelterCard.php';
+require_once '../views/components/petCard.php';
+
 
 ?>
 
@@ -13,5 +25,46 @@ new App\Template('empty');
 <a class="navbar-brand" href="/../pets/create.php" style="display:flex; justify-content: space-evenly; text-decoration: none; color: white;"></a>
 </div>
 <h3 class="section-title">Feltöltések:</h3>
+<?php
+if($userShelters){
+    foreach ($userShelters as $shelter) {
+        $ownsByTheUserBool = $user ? $shelter->ownsByTheUser($user->id) : false;
+        $type = $user ? $user->type :false;
+        shelter_card([
+            'shelter_name' => $shelter->shelter_name,
+            'city' => $shelter->city,
+            'img' => $shelter->img,
+            'slug' => $shelter->shelter_slug,
+            'auth' => $ownsByTheUserBool,
+            'type' => $type
+        ]);
+    }
+}else{
+    echo 'Nincsen megjelníthető menhely!';
+}
+?>
 
+<hr>
+<h3 class="section-title">Kisállatok:</h3>
+<?php
+if($userPets){
+    foreach ($userPets as $pet) {
+        $ownsByTheUserBool = $user ? $pet->ownsByTheUser($user->id) : false;
+        $type = $user ? $user->type :false;
+        post_item([
+            'img' => $pet->img,
+            'pet_name' => $pet->pet_name,
+            'pet_breed' => $pet->pet_breed,
+            'pet_gender' => $pet->pet_gender,
+            'pet_age' => $pet->pet_age,
+            'pet_description' => $pet->description,
+            'slug' => $pet->slug,
+            'auth' => $ownsByTheUserBool,
+            'type' => $type
+        ]);
+    }
+}else{
+    echo 'Nincsen megjelníthető menhely!';
+}
+?>
 </main>
