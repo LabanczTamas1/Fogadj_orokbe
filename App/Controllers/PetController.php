292c716c473d6d9ffca2c409827130332e644cd1
szacsihu <szacsihu@gmail.com>
2024-11-17 21:16:29 +0100

    CRUD update pets

--- App/Controllers/PetController.php
+++ App/Controllers/PetController.php
@@ -35,4 +35,47 @@ class PetController {
            return false;
         }
     }
+	
+	public function updatePet($arr){
+        global $user;
+        $petModel = new Pet();
+        $pet = $petModel->getItemById($arr['id']);
+        $pet->set('postname' ,str_replace("'", "", $arr['postname']));
+        $pet->set('slug', Tools::slugify($arr['postname']));
+        $pet->set('shelter_id' ,intval( $arr['shelter_id']));
+        $pet->set('user_id', $user->id);
+        $pet->set('pet_name' ,str_replace("'", "", $arr['pet_name']));
+        $pet->set('pet_gender' ,str_replace("'", "", $arr['pet_gender']));
+        $pet->set('pet_breed' ,str_replace("'", "", $arr['pet_breed']));
+        $pet->set('pet_status', intval($arr['pet_status']));
+        $pet->set('pet_age', intval($arr['pet_age']));
+        $pet->set('description', str_replace("'", "", $arr['description']));
+
+
+        if ($_FILES && $_FILES['img']['name'] != "") {
+
+            $newImage = Image::UpdateImage($pet->img, $_FILES, '/files/pet_image');
+
+            if (is_array($newImage)) {
+                $errors = $newImage;
+
+                Tools::FlashMessage('Hiba a feltöltéssel: ' . $errors[0], 'danger');
+                return false;
+            }
+            $pet->set('img', $newImage);
+        }else{
+            $pet->set( 'img',$pet->img);
+        }
+
+        try {
+            if ($pet->update()) {
+                Tools::FlashMessage($pet->postname . ' módosítva', 'success');
+                header("Location: /");
+            }
+        } catch (\Exception $e) {
+            die();
+        }
+
+
+    }
 }
--- pets/edit.php
+++ pets/edit.php
@@ -10,14 +10,18 @@ $sheltersModel= new App\Models\Shelter;
 $shelters = $sheltersModel->getItemsBy('user_id',App\Helper::user()->id);
 $petModel = new App\Models\Pet;
 $pet = $petModel->getItemBy('slug',$_GET['slug']);
-
+if(isset($_POST['submit'])){
+    $petController = new App\Controllers\PetController;
+    $petController->updatePet($_POST);
+    header("Location:/");
+}
 ?>
 
 <!-- Add back the form structure here -->
 <h4>Üdv, <?= App\Helper::user()->username ?>!</h4>
 <h6>Feltöltés</h6>
 
-<form class="upload-form" method="POST" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+<form class="upload-form" method="POST" enctype="multipart/form-data">
     <?php if (!empty($errors)) : ?>
         <?php foreach ($errors as $error) : ?>
             <div class="alert alert-danger">
@@ -30,7 +34,7 @@ $pet = $petModel->getItemBy('slug',$_GET['slug']);
         <label for="image-upload" class="upload-box">
             <i class="upload-icon"><i class="fa-solid fa-arrow-up-from-bracket"></i></i>
             <span>Kép feltöltése</span>
-            <input type="file" name="img" id="image-upload" value="<?=$pet->img;?>" required/>
+            <input type="file" name="img" id="image-upload" value="<?=$pet->img;?>" />
         </label>
         <div id="preview-container">
     <img id="preview-image" src="" alt="Image Preview" style="display:none; max-width: 100%; height: auto;"/>
@@ -40,6 +44,8 @@ $pet = $petModel->getItemBy('slug',$_GET['slug']);
 
     <input type="text" name="postname" class="form-input" value="<?=$pet->postname;?>" required/>
     <input type="text" name="user_id" class="form-input" value="<?=$pet->user_id;?>" required hidden/>
+    <input type="text" name="pet_status" class="form-input" value="<?=$pet->pet_status;?>" required hidden/>
+    <input type="text" name="id" class="form-input" value="<?=$pet->id;?>" required hidden/>
     <input type="text" name="user_id" class="form-input" value="<?=$pet->pet_status;?>" required hidden/>
     <input type="text" name="pet_name" class="form-input" value="<?=$pet->pet_name;?>" required/>
     <input type="text" name="pet_gender" class="form-input" value="<?=$pet->pet_gender;?>" required/>
@@ -50,7 +56,7 @@ $pet = $petModel->getItemBy('slug',$_GET['slug']);
                 <option <?php if($pet->shelter_id == $shelter->id) echo 'selected'?> value="<?=$shelter->id?>" ><?=$shelter->shelter_name?></option>
             <?php endforeach;?>
     </select>
-    <textarea name="description" class="form-input" value="<?=$pet->description;?>" required></textarea>
+    <textarea name="description" class="form-input" value="<?=$pet->description;?>" required><?=$pet->description;?></textarea>
     <input type="submit" name="submit" class="upload-button" value="Feltöltés">
 </form>
 <script src="../files/js/image-preview.js"></script>
