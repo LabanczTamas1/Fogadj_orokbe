e363c6823b2d500a25d4049dd0d76a5f0844f352
Labancz Tamás <labancztamas@yahoo.hu>
2024-11-19 14:42:46 +0100

    PetCard Admin buttons UI fix

--- views/components/petCard.php
+++ views/components/petCard.php
@@ -10,14 +10,16 @@ function post_item(array $pet_card)
             <div class="pet-gender">Nem:<?=$pet_card['pet_gender']?></div>
             <div class="pet-age">Kor:<?=$pet_card['pet_age']?></div>
             <div class="description" style="color: white;">Leírás:<?=$pet_card['pet_description']?></div>
-            <div class="linky">
+            <div class="linky-pet">
             <button onclick="document.navigateTo('/pets/<?= $pet_card['slug'] ?>/form'); return false;" class="shelter-button">Örökbefogadás</button>
 
             </div>
             <?php if ($pet_card['auth'] || $pet_card['type'] == 'Developer'): ?>
-                <button onclick="document.navigateTo('/pets/<?= $pet_card['slug'] ?>/edit'); return false;">módosítás</button>
-                <button onclick="document.navigateTo('/pets/<?= $pet_card['slug'] ?>/delete'); return false;">törlés</button>
-            <?php endif ?>
+                <div id="card-buttons" style="display: flex; justify-content: space-between; width: 100%;">
+                <button onclick="document.navigateTo('/pets/<?= $pet_card['slug'] ?>/edit'); return false;" style="background-color: #B0846D; margin-left: 5px;">módosítás ↻</button>
+                <button onclick="document.navigateTo('/pets/<?= $pet_card['slug'] ?>/delete'); return false;" style="background-color: #B0846D; margin-right: 25px;">🗑</button>
+            </div>
+                <?php endif ?>
         </div>
     </div>
 <?php
