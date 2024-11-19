6f5a959b9bc273c3876b1ee24e78370a38c4a785
Labancz Tamás <labancztamas@yahoo.hu>
2024-11-19 15:57:13 +0100

    Landing page upgrades

--- views/layouts/home_layout.php
+++ views/layouts/home_layout.php
@@ -12,7 +12,7 @@
 <style>
 
 footer{
-  margin-top: 550px;
+  margin-top: 50px;
 }
 
 </style>
@@ -31,6 +31,37 @@ footer{
   <div class="overlay-text"><div class="sub-title">  <img src="/files/img/SPEEDY2-768x1253.jpg" alt="Person holding a dog" width="400px" height="600px">„Segítsd a menhely lakóit – adományozz egy boldogabb jövőért!</div>
   Egy kis támogatás is sokat jelent. Legyen szó ételről, gyógyszerről vagy játékokról, az adományod közvetlenül az állatok jólétét szolgálja.”</div>
   </div>
+  <div class="extra-section">
+  
+
+  <div class="cool-cards">
+        <div class="card">
+            <img src="/files/img/SPEEDY2-768x1253.jpg" alt="Cute Animal 1">
+            <div class="card-content">
+                <h3>Join Us</h3>
+                <p>Help us bring a change by joining our community of animal lovers.</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/files/img/SPEEDY2-768x1253.jpg" alt="Cute Animal 2">
+            <div class="card-content">
+                <h3>Donate Now</h3>
+                <p>Your donations go directly to the care of our furry friends.</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/files/img/SPEEDY2-768x1253.jpg" alt="Cute Animal 3">
+            <div class="card-content">
+                <h3>Adopt</h3>
+                <p>Give a forever home to a pet in need of love and care.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+  
+</div>
+
   </div>
 
   <?php $this->include('components/footer') ?>
