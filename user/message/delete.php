a2862f671b52fe8bd2b257c0068c54604a78a1ac
Dobrosi Gergő <dobrosi.gergo13@gmail.com>
2024-12-03 14:58:26 +0100

    message user delete

--- user/message/delete.php
+++ user/message/delete.php
@@ -1,34 +1,21 @@
 <?php
 Use App\Tools;
 use App\Helper;
-require_once __DIR__. '/../lib/autoload.php';
-$user = Helper::user();
-if($user->email != $_GET["email"]){
+require_once __DIR__. '/../../lib/autoload.php';
+$formModel = new App\Models\Form;
+$form = $formModel->getItemById($_GET["id"]);
+if(Helper::user()->id != $form->user_id){
+    header("Lcoation:/");
     Tools::FlashMessage("Not authorized","danger");
-    header("Location: /");
     exit();
 }
-$formModel = new App\Models\Form;
-$forms = $formModel->getItemsBy('user_id',$user->id);
-if ($user->type == "Shleter"){
-    $shelterModel = new App\Models\Shelter;
-    $shelters = $shelterModel->getItemsBy('user_id',$user->id);
-    $petModel = new App\Models\Pet;
-    $pets = $petModel->getItemsBy('user_id',$user->id);
-}
-$db = new APp\Database;
+$db = new App\Database;
 try{
-    if($user){
-        if($forms){ 
-            $db->delete_all("form","user_id",$user->id);
-        }if($shelters){
-            $db->delete_all("shelter","user_id",$user->id);
-        }if($pets){
-            $db->delete_all("pet_posts","user_id",$user->id);
-        }
-        $user->delete();
-        Tools::FlashMessage("User deleted","success");
-    }
+   if($form){
+    $db->delete_all("form","id",$form->id);
+    Tools::FlashMessage("Message has been deleted.","success");
+    header("Location:/user/messages");
+   }
     
 
 }catch(Exception $e){
