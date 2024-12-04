fdbc9b6f40f81d9be0765604b854b08e5bead344
Labancz Tamás <labancztamas@yahoo.hu>
2024-12-05 00:05:38 +0100

    IncomingMessages Test added

--- cypress/e2e/UI-Tests/IncomingMessages.cy.js
+++ cypress/e2e/UI-Tests/IncomingMessages.cy.js
@@ -65,7 +65,7 @@ describe('User Login and Upload Flow', () => {
 
 });
 
-describe('User Login and Upload Flow', () => {
+describe('User Login and Message Upload Flow', () => {
 
     it('should login successfully and upload the form', () => {
         // Step 1: Login
@@ -105,3 +105,37 @@ describe('User Login and Upload Flow', () => {
     });
 
 });
+
+describe('Check message', () => {
+
+    it('should login successfully and upload the form', () => {
+        // Step 1: Login
+        cy.visit('http://localhost/userhandle/login.php');
+        
+        // Fill in the email
+        cy.get('#email_value').type('a@a'); // Replace with a valid test email
+
+        // Fill in the password
+        cy.get('#passwd_value').type('aladartest'); // Replace with a valid test password
+
+        // Submit the form
+        cy.get('button[name="userlogin"]').click({ force: true });
+
+        // Assert success message appears
+        cy.contains('Sikeres bejelentkezés').should('be.visible'); // Check for success text
+
+        // Step 2: Navigate to the upload page
+        cy.get('body > nav > div.nav-links > a:nth-child(4)').click();
+
+        cy.get('body > main > div.petsandshelter > a:nth-child(4)').click();
+
+        cy.contains('wq').should('be.visible');
+
+    });
+
+    
+       
+
+
+});
+
