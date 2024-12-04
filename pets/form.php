9634f9f590c1b85aa67e7647dab3006d023d9621
Labancz Tamás <labancztamas@yahoo.hu>
2024-12-04 11:34:16 +0100

    Message form design fix

--- pets/form.php
+++ pets/form.php
@@ -15,6 +15,16 @@ if(isset($_POST['submit'])){
 }
 ?>
 
+<style>
+    #message-label{
+        color: white;
+    }
+
+    .container-pet > *{
+        color: white;
+    }
+</style>
+
 <div class="container-pet">
 <?= $pet->postname;?>
 <?= $pet->pet_name;?>
@@ -27,15 +37,25 @@ if(isset($_POST['submit'])){
 </div>
 <hr>
 
-<form class="upload-form" method="POST" enctype="multipart/form-data">
-    <div>
+<form class="upload-form" method="POST" enctype="multipart/form-data" style="display: flex; align-items: center; justify-content: center; height: 50vh; width:100%;">
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
         <input type="text" name="user_id" class="form-input" value="<?=Helper::user()->id;?>" required hidden/>
         <input type="text" name="shelter_id" class="form-input" value="<?=$shelter->id;?>" required hidden />
         <input type="text" name="pet_id" class="form-input" value="<?=$pet->id;?>" required hidden />
-
-        <input type="text" name="fullname" class="form-input" value="<?=Helper::user()->username;?>" required readonly="true"/>
-        <input type="text" name="email" class="form-input" value="<?=Helper::user()->email;?>" required readonly="true"/>
-        <textarea name="message" class="form-input"  placeholder="Üzenet a menhely számára: "required></textarea>
+        
+        <label id="message-label">Neved (Automatikusan kitöltve)</label>
+        <input type="text" name="fullname" class="form-input" value="<?=Helper::user()->username;?>" required readonly="true" style="margin:0px !important;"/>
+        <label id="message-label">Emailed (Automatikusan kitöltve)</label>
+        <input type="text" name="email" class="form-input" value="<?=Helper::user()->email;?>" required readonly="true" style="margin:0px;"/>
+        <label id="message-label">Üzenet a menhelynek</label>
+        <textarea name="message" class="form-input"  placeholder="Üzenet a menhely számára: " style="width: 240px;
+    height: 100%;
+    color: black;
+    background-color: rgba(256, 256, 256, 0.48);
+    border-radius: 8px;
+    padding: 16px;
+    
+    border: 1px rgb(217, 217, 217) solid;" required></textarea>
     </div>
 
     <input type="submit" name="submit" class="upload-button" value="Elküldés">
